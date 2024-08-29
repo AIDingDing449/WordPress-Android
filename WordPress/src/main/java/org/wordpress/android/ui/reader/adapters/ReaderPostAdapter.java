ce90d5d5d748d9805ebace83b6ff67bcb79ecb32
AIDingDing449 <AIDingDing449@outlook.com>
2024-08-29 03:34:35 +0000

     No one can whistle a symphony. It takes a whole orchestra to play it. 

--- WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderPostAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderPostAdapter.java
@@ -40,13 +40,11 @@ import org.wordpress.android.ui.reader.ReaderTypes.ReaderPostListType;
 import org.wordpress.android.ui.reader.actions.ReaderActions;
 import org.wordpress.android.ui.reader.actions.ReaderTagActions;
 import org.wordpress.android.ui.reader.discover.ReaderCardUiState;
-import org.wordpress.android.ui.reader.discover.ReaderCardUiState.ReaderPostNewUiState;
 import org.wordpress.android.ui.reader.discover.ReaderCardUiState.ReaderPostUiState;
 import org.wordpress.android.ui.reader.discover.ReaderPostCardActionType;
 import org.wordpress.android.ui.reader.discover.ReaderPostMoreButtonUiStateBuilder;
 import org.wordpress.android.ui.reader.discover.ReaderPostUiStateBuilder;
 import org.wordpress.android.ui.reader.discover.viewholders.ReaderPostNewViewHolder;
-import org.wordpress.android.ui.reader.discover.viewholders.ReaderPostViewHolder;
 import org.wordpress.android.ui.reader.models.ReaderBlogIdPostId;
 import org.wordpress.android.ui.reader.utils.ReaderAnnouncementHelper;
 import org.wordpress.android.ui.reader.tracker.ReaderTab;
@@ -68,7 +66,6 @@ import org.wordpress.android.util.NetworkUtils;
 import org.wordpress.android.util.NetworkUtilsWrapper;
 import org.wordpress.android.util.SiteUtils;
 import org.wordpress.android.util.ToastUtils;
-import org.wordpress.android.util.config.ReaderImprovementsFeatureConfig;
 import org.wordpress.android.util.extensions.ContextExtensionsKt;
 import org.wordpress.android.util.image.BlavatarShape;
 import org.wordpress.android.util.image.ImageManager;
@@ -98,8 +95,6 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     private final int mPhotonWidth;
     private final int mPhotonHeight;
     private final int mAvatarSzSmall;
-
-    private boolean mCanRequestMorePosts;
 
     @NonNull private final ReaderTypes.ReaderPostListType mPostListType;
     @NonNull private String mSource;
@@ -138,7 +133,6 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Inject ReaderPostUiStateBuilder mReaderPostUiStateBuilder;
     @Inject ReaderPostMoreButtonUiStateBuilder mReaderPostMoreButtonUiStateBuilder;
     @Inject ReaderTracker mReaderTracker;
-    @Inject ReaderImprovementsFeatureConfig mReaderImprovementsFeatureConfig;
     @Inject ReaderAnnouncementHelper mReaderAnnouncementHelper;
 
     public String getSource() {
@@ -281,28 +275,23 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
                 return new GapMarkerViewHolder(new ReaderGapMarkerView(context));
 
             case VIEW_TYPE_XPOST:
-                final int layoutRestId = mReaderImprovementsFeatureConfig.isEnabled()
-                        ? R.layout.reader_cardview_xpost_new : R.layout.reader_cardview_xpost;
-                postView = LayoutInflater.from(context).inflate(layoutRestId, parent, false);
+                postView = LayoutInflater.from(context).inflate(R.layout.reader_cardview_xpost, parent, false);
                 return new ReaderXPostViewHolder(postView);
             case VIEW_TYPE_REMOVED_POST:
-                final int layoutResId = mReaderImprovementsFeatureConfig.isEnabled()
-                        ? R.layout.reader_cardview_removed_post_new : R.layout.reader_cardview_removed_post;
-                postView = LayoutInflater.from(context).inflate(layoutResId, parent, false);
+                postView = LayoutInflater.from(context).inflate(
+                        R.layout.reader_cardview_removed_post, parent, false
+                );
                 return new ReaderRemovedPostViewHolder(postView);
             default:
-                return mReaderImprovementsFeatureConfig.isEnabled()
-                        ? new ReaderPostNewViewHolder(mUiHelpers, mImageManager, mReaderTracker, mNetworkUtilsWrapper,
-                        parent)
-                        : new ReaderPostViewHolder(mUiHelpers, mImageManager, mReaderTracker, parent);
+                return new ReaderPostNewViewHolder(
+                        mUiHelpers, mImageManager, mReaderTracker, mNetworkUtilsWrapper, parent
+                );
         }
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof ReaderPostViewHolder) {
-            renderPost(position, (ReaderPostViewHolder) holder, false);
-        } else if (holder instanceof ReaderPostNewViewHolder) {
+        if (holder instanceof ReaderPostNewViewHolder) {
             renderPostNew(position, (ReaderPostNewViewHolder) holder, false);
         } else if (holder instanceof ReaderXPostViewHolder) {
             renderXPost(position, (ReaderXPostViewHolder) holder);
@@ -461,116 +450,6 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         }
     }
 
-    private void renderPost(final int position, final ReaderPostViewHolder holder, boolean showMoreMenu) {
-        final ReaderPost post = getItem(position);
-        ReaderPostListType postListType = getPostListType();
-        if (post == null) {
-            return;
-        }
-        Context ctx = holder.getViewContext();
-        Function3<Long, Long, ReaderPostCardActionType, Unit> onButtonClicked =
-                (postId, blogId, type) -> {
-                    mOnPostListItemButtonListener.onButtonClicked(post, type);
-                    renderPost(position, holder, false);
-                    return Unit.INSTANCE;
-                };
-        Function2<Long, Long, Unit> onItemClicked = (postId, blogId) -> {
-            if (mPostSelectedListener != null) {
-                mPostSelectedListener.onPostSelected(post);
-            }
-            return Unit.INSTANCE;
-        };
-        Function1<ReaderCardUiState, Unit> onItemRendered = (item) -> {
-            checkLoadMore(position);
-
-            // if we haven't already rendered this post and it has a "railcar" attached to it, add it
-            // to the rendered list and record the TrainTracks render event
-            if (post.hasRailcar() && !mRenderedIds.contains(post.getPseudoId())) {
-                mRenderedIds.add(post.getPseudoId());
-                mReaderTracker.trackRailcar(post.getRailcarJson());
-            }
-            return Unit.INSTANCE;
-        };
-        Function2<Long, Long, Unit> onDiscoverSectionClicked = (postId, blogId) -> {
-            ReaderPostDiscoverData discoverData = post.getDiscoverData();
-            switch (discoverData.getDiscoverType()) {
-                case EDITOR_PICK:
-                    if (mPostSelectedListener != null) {
-                        mPostSelectedListener.onPostSelected(post);
-                    }
-                    break;
-                case SITE_PICK:
-                    if (discoverData.getBlogId() != 0) {
-                        ReaderActivityLauncher.showReaderBlogPreview(
-                                ctx,
-                                discoverData.getBlogId(),
-                                post.isFollowedByCurrentUser,
-                                mSource,
-                                mReaderTracker
-                        );
-                    } else if (discoverData.hasBlogUrl()) {
-                        ReaderActivityLauncher.openUrl(ctx, discoverData.getBlogUrl());
-                    }
-                    break;
-                case OTHER:
-                    // noop
-                    break;
-            }
-            return Unit.INSTANCE;
-        };
-        Function1<ReaderPostUiState, Unit> onMoreButtonClicked = (uiState) -> {
-            renderPost(position, holder, true);
-            return Unit.INSTANCE;
-        };
-
-        Function1<ReaderPostUiState, Unit> onMoreDismissed = (uiState) -> {
-            renderPost(position, holder, false);
-            return Unit.INSTANCE;
-        };
-
-        Function2<Long, Long, Unit> onVideoOverlayClicked = (postId, blogId) -> {
-            ReaderActivityLauncher.showReaderVideoViewer(ctx, post.getFeaturedVideo());
-            return Unit.INSTANCE;
-        };
-
-        Function0<Unit> onPostHeaderClicked = () -> {
-            ReaderActivityLauncher.showReaderBlogPreview(
-                    ctx,
-                    post,
-                    mSource,
-                    mReaderTracker
-            );
-            return Unit.INSTANCE;
-        };
-
-        Function1<String, Unit> onTagItemClicked = (tagSlug) -> {
-            // noop
-            return Unit.INSTANCE;
-        };
-
-        ReaderPostUiState uiState = mReaderPostUiStateBuilder
-                .mapPostToUiStateBlocking(
-                        mSource,
-                        post,
-                        false,
-                        mPhotonWidth,
-                        mPhotonHeight,
-                        postListType,
-                        onButtonClicked,
-                        onItemClicked,
-                        onItemRendered,
-                        onDiscoverSectionClicked,
-                        onMoreButtonClicked,
-                        onMoreDismissed,
-                        onVideoOverlayClicked,
-                        onPostHeaderClicked,
-                        onTagItemClicked,
-                        showMoreMenu ? mReaderPostMoreButtonUiStateBuilder
-                                .buildMoreMenuItemsBlocking(post, false, false, onButtonClicked) : null
-                );
-        holder.onBind(uiState);
-    }
-
     // TODO update the viewholder to the new one
     private void renderPostNew(final int position, final ReaderPostNewViewHolder holder, boolean showMoreMenu) {
         final ReaderPost post = getItem(position);
@@ -628,12 +507,12 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             }
             return Unit.INSTANCE;
         };
-        Function1<ReaderPostNewUiState, Unit> onMoreButtonClicked = (uiState) -> {
+        Function1<ReaderPostUiState, Unit> onMoreButtonClicked = (uiState) -> {
             renderPostNew(position, holder, true);
             return Unit.INSTANCE;
         };
 
-        Function1<ReaderPostNewUiState, Unit> onMoreDismissed = (uiState) -> {
+        Function1<ReaderPostUiState, Unit> onMoreDismissed = (uiState) -> {
             renderPostNew(position, holder, false);
             return Unit.INSTANCE;
         };
@@ -653,8 +532,8 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             return Unit.INSTANCE;
         };
 
-        ReaderPostNewUiState uiState = mReaderPostUiStateBuilder
-                .mapPostToNewUiStateBlocking(
+        ReaderPostUiState uiState = mReaderPostUiStateBuilder
+                .mapPostToUiStateBlocking(
                         mSource,
                         post,
                         mPhotonWidth,
@@ -677,8 +556,7 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
      * if we're nearing the end of the posts, fire request to load more
      */
     private void checkLoadMore(int position) {
-        if (mCanRequestMorePosts
-            && mDataRequestedListener != null
+        if (mDataRequestedListener != null
             && (position >= getItemCount() - 1)) {
             mDataRequestedListener.onRequestData();
         }
@@ -939,7 +817,6 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     private class LoadPostsTask extends AsyncTask<Void, Void, Boolean> {
         private ReaderPostList mAllPosts;
 
-        private boolean mCanRequestMorePostsTemp;
         private int mGapMarkerPositionTemp;
 
         @Override
@@ -954,21 +831,17 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            int numExisting = 0;
             switch (getPostListType()) {
                 case TAG_PREVIEW:
                 case TAG_FOLLOWED:
                 case SEARCH_RESULTS:
                     mAllPosts = ReaderPostTable.getPostsWithTag(mCurrentTag, MAX_ROWS, EXCLUDE_TEXT_COLUMN);
-                    numExisting = ReaderPostTable.getNumPostsWithTag(mCurrentTag);
                     break;
                 case BLOG_PREVIEW:
                     if (mCurrentFeedId != 0) {
                         mAllPosts = ReaderPostTable.getPostsInFeed(mCurrentFeedId, MAX_ROWS, EXCLUDE_TEXT_COLUMN);
-                        numExisting = ReaderPostTable.getNumPostsInFeed(mCurrentFeedId);
                     } else {
                         mAllPosts = ReaderPostTable.getPostsInBlog(mCurrentBlogId, MAX_ROWS, EXCLUDE_TEXT_COLUMN);
-                        numExisting = ReaderPostTable.getNumPostsInBlog(mCurrentBlogId);
                     }
                     break;
                 case TAGS_FEED:
@@ -978,10 +851,6 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             if (mPosts.isSameListWithBookmark(mAllPosts)) {
                 return false;
             }
-
-            // if we're not already displaying the max # posts, enable requesting more when
-            // the user scrolls to the end of the list
-            mCanRequestMorePostsTemp = (numExisting < ReaderConstants.READER_MAX_POSTS_TO_DISPLAY);
 
             // determine whether a gap marker exists - only applies to tagged posts
             mGapMarkerPositionTemp = getGapMarkerPosition();
@@ -1022,7 +891,6 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         protected void onPostExecute(Boolean result) {
             if (result) {
                 ReaderPostAdapter.this.mGapMarkerPosition = mGapMarkerPositionTemp;
-                ReaderPostAdapter.this.mCanRequestMorePosts = mCanRequestMorePostsTemp;
                 mPosts.clear();
                 mPosts.addAll(mAllPosts);
                 notifyDataSetChanged();
